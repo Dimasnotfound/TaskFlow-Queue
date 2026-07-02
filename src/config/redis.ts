5b8b15972212832232941e27ce294618c721e8c8
dimasnotfound <dp4369344@gmail.com>
2026-07-02 15:53:09 +0700

    fix: align redis client import

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,3 +1,4 @@
 import IORedis from 'ioredis';
 import { env } from './env.js';
-export const redis = new IORedis({ host: env.REDIS_HOST, port: env.REDIS_PORT, password: env.REDIS_PASSWORD, maxRetriesPerRequest: null });
+const RedisCtor = IORedis as unknown as new (options: object) => unknown;
+export const redis = new RedisCtor({ host: env.REDIS_HOST, port: env.REDIS_PORT, password: env.REDIS_PASSWORD, maxRetriesPerRequest: null });
